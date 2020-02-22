337d623b04a7d6a6f3617ae39f2edf244ee56a60
Chris Buteau <cbuteau@gmail.com>
2020-02-22 13:15:42 -0500

    enabled

---
+++ test/enabled.js
@@ -0,0 +1,23 @@
+var meas;
+describe('Exercise API', function() {
+  beforeAll(function(done) {
+    require(['meas'], function(measLoad) {
+      meas = measLoad;
+      meas.enable(false);
+      window.performance.clearMeasures();
+      done();
+    });
+  });
+
+  it ('RUn loop', function() {
+    meas.start('loop');
+
+    var array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+    for ( var i = 0; i < array.length; i++) {
+      console.log(array[i]);
+    }
+    meas.end('loop');
+    var measures = window.performance.getEntriesByType('measure');
+    expect(measures.length).toBe(0);
+  });
+});
