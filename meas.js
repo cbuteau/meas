41959c3ecb7855a09797e74b853b07fc3d80cca6
BUTEAU Christopher <CBU1@dsone.3ds.com>
2020-09-30 10:06:16 -0400

    current trackers were not being completed when replaced.

--- meas.js
+++ meas.js
@@ -148,6 +148,11 @@ TrackerSection.prototype = {
 
     var prefixedName = this.trackerPrefix + '.' + trackerName;
 
+    if (this.trackers[trackerName]) {
+      // at least end the current before replacing.
+      this.trackers[trackerName].endnmeas();
+    }
+
     this.trackers[trackerName] = new Tracker(this.perfPtr, prefixedName, options);
   },
 
