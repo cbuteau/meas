a917058518865cfbc03a83bda5d6d76634cac6f0
BUTEAU Christopher <CBU1@dsone.3ds.com>
2020-10-06 09:48:48 -0400

    browser getting killed

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,12 @@
 module.exports = function(config) {
   config.set({
 
+    browserNoActivityTimeout: 400000,
+    browserDisconnectTimeout: 10000,
+    captureTimeout: 210000,
+    browserDisconnectTolerance: 3,
+    processKillTimeout: 100000,
+
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
